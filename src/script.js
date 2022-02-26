1cc46639112a427cb5c41bae77de4ca7e6431135
ESPersonnel <w.ndiba@gmail.com>
2022-02-26 23:01:49 +0300

    Animated Particles: Math

--- src/script.js
+++ src/script.js
@@ -19,15 +19,53 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('textures/particles/2.png')
 
 /**
- * Test cube
+ * Particles
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+// Geometry
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 20000
+
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
+
+for(let i = 0; i < count * 3; i++)
+{
+  positions[i] = (Math.random() - 0.5) * 10
+  colors[i] = Math.random()
+}
+
+particlesGeometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3))
+particlesGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
+
+// Materials
+const particlesMaterial = new THREE.PointsMaterial()
+particlesMaterial.size = 0.1
+particlesMaterial.sizeAttenuation = true
+// particlesMaterial.color = new THREE.Color('#ff88cc')
+particlesMaterial.map = particleTexture
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
+// particlesMaterial.alphaTest = 0.001
+// particlesMaterial.depthTest = true
+particlesMaterial.depthWrite = false
+particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.vertexColors = true
+
+/**
+ * Points
+ */
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+// // Cube
+// const cube = new THREE.Mesh(
+//     new THREE.BoxBufferGeometry(),
+//     new THREE.MeshBasicMaterial()
+// )
+// scene.add(cube)
 
 /**
  * Sizes
@@ -81,6 +119,20 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update Particles
+    // particles.rotation.y = elapsedTime * 0.05
+    for(let i = 0; i < count; i++)
+    {
+      const i3 = i * 3
+
+    //   // Cylindrical spiral ultimate speed animation strong😂
+    //   particlesGeometry.attributes.position.array[i3 + 0] = Math.sin(elapsedTime + positions[i3 + 0]) / 2
+        const x = particlesGeometry.attributes.position.array[i3]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+
+    }
+    particlesGeometry.attributes.position.needsUpdate = true
 
     // Update controls
     controls.update()
